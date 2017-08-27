58dd043764a22f823513d18a171122bce374546a
Jay Welborn <jesse.welborn@gmail.com>
2017-08-27 22:08:04 +0200

    modeled contact data, tested magic method override

--- bingo/home/models.py
+++ bingo/home/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class Contact(models.Model):
+    """Model to store Contact Information.
+
+    Fields:
+        title: Title to display on browser tab header
+        facebook: URL for Facebook link on contact page
+        github: URL for GitHub link on contact page
+        linkedin: URL for LinkedIn link on contact page
+        twitter: URL for Twitter link on contact page
+        email: Creator's email address
+        contact_date: Date contact info was last update
+
+    References:
+
+        * none
+
+    """
+
+    class Meta:
+        verbose_name_plural = 'Contact'
+
+    title = models.CharField(max_length=30)
+    facebook = models.URLField(blank=True)
+    github = models.URLField(blank=True)
+    linkedin = models.URLField(blank=True)
+    twitter = models.URLField(blank=True)
+    email = models.EmailField()
+    contact_date = models.DateField(default=timezone.now)
+
+    def __str__(self):
+        return self.title
--- bingo/home/tests.py
+++ bingo/home/tests.py
@@ -1,3 +1,29 @@
 from django.test import TestCase
+from django.utils import timezone
+
+from .models import Contact
 
 # Create your tests here.
+class ContactModelTest(TestCase):
+    """
+    Tests for Contact Model
+    """
+
+    def setUp(self):
+        """
+        Create Instance(s) for tests
+        """
+        Contact.objects.get_or_create(title='title',
+                                      facebook='www.facebook.com',
+                                      github='www.github.com',
+                                      linkedin='www.linkedin.com',
+                                      twitter='www.twitter.com',
+                                      email='some_email@something.com',
+                                      contact_date=timezone.now())
+
+    def test_str_method(self):
+        """
+        __str__ method should return contact object's title
+        """
+        contact = Contact.objects.latest('contact_date')
+        self.assertEqual(contact.title, str(contact))
