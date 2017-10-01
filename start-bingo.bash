53fd30ec01c14a145286fb5603c1b50ce3936aff
jaywelborn <jesse.welborn@gmail.com>
2017-10-01 13:43:13 +0200

    trying to make useful automated startup

--- start-bingo.bash
+++ start-bingo.bash
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+# Executes within xterm script installed system-wide.
+
 sudo apt-get update;
 sudo apt-get upgrade -y;
 cd ~/Documents/projects/Bingo;
 source bingoenv/bin/activate;
 cd bingo;
-# python manage.py test;
+python manage.py test;
