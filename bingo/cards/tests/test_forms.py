813a84c803d55fd950dc45b5265f1d08b579a3db
jaywelborn <jesse.welborn@gmail.com>
2017-10-18 19:02:38 +0200

    added test for square create form

--- bingo/cards/tests/test_forms.py
+++ bingo/cards/tests/test_forms.py
@@ -1,8 +1,8 @@
 from django.contrib.auth.models import User
 from django.test import TestCase
 
-from cards.forms import BingoCardForm
-from cards.models import BingoCard
+from cards.forms import BingoCardForm, BingoSquareForm
+from cards.models import BingoCard, BingoCardSquare
 
 import pdb
 
@@ -79,3 +79,80 @@ class BingoCardFormtests(TestCase):
         self.assertTrue(card)
         self.assertEqual(card, test_card)
         self.assertEqual(card.creator, self.user)
+
+
+class BingoSquareFormtests(TestCase):
+    """Tests for BingoCardForm
+
+    Methods:
+        setUp: Create test data for form
+        test_helper: Crispy helper should exist and have appropriate attributes
+        test_form_accepts_valid_data: Form should accept valid data
+        test_card_created_correctly: New BingoSquare should be created with
+        data from form, and defaults for attributes not included in the form.
+
+    References:
+
+    * http://test-driven-django-development.readthedocs.io/en/latest/05-forms.html
+
+    """
+
+    def setUp(self):
+        """
+        Create Test data.
+        """
+
+        # User to serve as card's creator
+        self.user = User.objects.create(
+            username='CardSquareTestUser',
+            email='squaretest@gmail.com'
+        )
+        self.user.set_password('squaref0rm!')
+        self.user.save()
+
+        self.card = BingoCard.objects.get_or_create(
+            title='SquareFormTest',
+            creator=self.user,
+        )[0]
+        self.card.save()
+
+        self.data = {
+            'text': 'CardSquareTest',
+            'card': str(self.card.id),
+        }
+
+    def test_helper(self):
+        """
+        Form should get crispy helper attributes when instantiated.
+        """
+
+        form = BingoSquareForm()
+
+        self.assertTrue(form.helper)
+        self.assertEqual(form.helper.form_id, 'bingo_square_form')
+        self.assertEqual(form.helper.form_method, 'post')
+        self.assertEqual(form.helper.form_action, '.')
+
+    def test_form_accepts_valid_data(self):
+        """
+        Form should accept and process valid data.
+        """
+        form = BingoSquareForm(self.data)
+        self.assertTrue(form.is_valid())
+
+    def test_square_created_correctly(self):
+        """
+        Form should create new BingoCard properly.
+        """
+
+        # instantiate form, and save it
+        form = BingoSquareForm(self.data)
+        if form.is_valid():
+            test_square = form.save()
+
+        # check card in DB against card returned by form
+        square = BingoCardSquare.objects.get(text='CardSquareTest')
+        self.assertTrue(square)
+        self.assertEqual(square, test_square)
+        self.assertEqual(square.card, self.card)
+        self.assertEqual(square.card.creator, self.user)
