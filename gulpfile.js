3edb30d0a8cd49da290b9ac46eda0826b67f2242
jaywelborn <jesse.welborn@gmail.com>
2017-11-01 08:45:03 +0100

    got gulp working with sass and django server. still need to add autoprefixer and browsersync

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var concat = require('gulp-concat');
+
+gulp.task('django', function() {
+    const spawn = require('child_process').spawn;
+    return spawn('python', ['bingo/manage.py', 'runserver'])
+        .stderr.on('data', (data) => {
+        console.log(`${data}`);
+    });
+});
+
+gulp.task('styles', function() {
+    gulp.src('./bingo/**/main.sass')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(concat('style.css'))
+        .pipe(gulp.dest('./bingo/static/css/'));
+});
+
+gulp.task('watch-sass', function() {
+    gulp.watch('./bingo/home/static/sass/main.sass',['styles']);
+})
+
+gulp.task('default', ['django', 'watch-sass']);
