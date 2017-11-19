7e607601f263cd7208739432ff07883661fa4dfc
jaywelborn <jesse.welborn@gmail.com>
2017-11-19 12:23:29 +0100

    added new password form for social logins with no password

--- bingo/auth_extension/views.py
+++ bingo/auth_extension/views.py
@@ -1,7 +1,8 @@
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, update_session_auth_hash
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.forms import AdminPasswordChangeForm, PasswordChangeForm
+from django.contrib.auth.forms import (AdminPasswordChangeForm,
+                                       PasswordChangeForm)
 from django.contrib.auth.mixins import LoginRequiredMixin as LRM
 from django.contrib.auth.models import User
 from django.contrib.messages.views import SuccessMessageMixin
