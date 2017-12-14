0a48f9a6de236f18d4e7fb564a3c87c8aa29f513
jaywelborn <jesse.welborn@gmail.com>
2017-12-14 20:21:29 +0100

    converted to Generic Class-Based API Views

--- bingo/api/views.py
+++ bingo/api/views.py
@@ -1,13 +1,11 @@
-from rest_framework import mixins, generics
+from rest_framework import generics
 
 from home.models import Contact
 
 from .serializers import ContactSerializer
 
 
-class ContactList(mixins.ListModelMixin,
-                  mixins.CreateModelMixin,
-                  generics.GenericAPIView):
+class ContactList(generics.ListCreateAPIView):
     """
     List all contact objects, or create a new contact object.
     """
@@ -15,29 +13,11 @@ class ContactList(mixins.ListModelMixin,
     queryset = Contact.objects.all()
     serializer_class = ContactSerializer
 
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
 
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
-
-
-class ContactDetail(mixins.RetrieveModelMixin,
-                    mixins.UpdateModelMixin,
-                    mixins.DestroyModelMixin,
-                    generics.GenericAPIView):
+class ContactDetail(generics.RetrieveUpdateDestroyAPIView):
     """
     Retrieve, update, or delete a contact object.
     """
 
     queryset = Contact.objects.all()
     serializer_class = ContactSerializer
-
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
-
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
