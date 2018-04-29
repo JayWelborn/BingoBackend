dfa5cb901aeb6a5a57a67de359171e4e9765bd46
jaywelborn <jesse.welborn@gmail.com>
2018-04-29 22:46:19 +0200

    changed settings to allow token authenticaiton

--- bingo/bingo/settings.py
+++ bingo/bingo/settings.py
@@ -237,7 +237,11 @@ SOCIAL_AUTH_RAISE_EXCEPTIONS = False
 # Rest API Settings
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10
+    'PAGE_SIZE': 10,
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ),
 }
 
 # Allow API requests from the following URLs
